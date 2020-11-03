91178335c961aa6da3c15fb5fbaabaeb88655e0d
pritijha09 <jhapriti09@gmail.com>
2020-11-03 20:58:01 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component, VERSION } from "@angular/core";
+
+@Component({
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  list = [];
+  name = "Angular " + VERSION.major;
+  public uuid: string;
+  label2: string;
+  label3: string;
+  label4: string;
+
+  add() {
+    if (this.list.some(ele => ele.uuid === this.uuid)) {
+      alert("Object found inside the array.");
+      let index = this.list.findIndex(ele => ele.uuid === this.uuid);
+      this.list.splice(index, 1);
+    } else {
+      alert("Object not found.");
+      this.list.push({
+        uuid: this.uuid,
+        label2: this.label2,
+        label3: this.label3,
+        label4: this.label4
+      });
+    }
+
+    //if(this.list)
+  }
+}
